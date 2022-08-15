2e61a834e8ac784ced48fc97237e890e76c487be
vuongvg <vuongcdt1@gmail.com>
2022-08-15 15:58:48 +0700

    fix catchError handleError env

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+// require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
@@ -7,7 +7,6 @@ const { editData } = require("./editData");
 const router = require("./routers");
 const { notFoundMdw } = require("./routers/notFoundMdw");
 const { errorHandleMdw } = require("./middlewares/errorHandleMdw");
-const url = require("url");
 
 const port = process.env.PORT || 5001;
 console.log("process.env.PORT:", process.env.PORT, process.env.MONGODB_URI);
@@ -21,21 +20,14 @@ app.use(
       // origin:"http://mywebsite.vn"
    })
 );
-const mdw = (req, res, next) => {
-   if (!req.query.name) throw new Error('aaaaaaaaaaaaaaaa')
-   next();
-};
+
 app.use(morgan("dev"));
 app.get("/", (req, res) => {
    res.send("Sever is runing: " + process.env.MONGODB_URI);
 });
-app.get("/test", mdw, (req, res) => res.send("TEST"));
 app.use("/api", router);
 app.use(notFoundMdw);
 app.use(errorHandleMdw);
-
-// console.log(url.parse(process.env.MONGODB_URI));
-// console.log(url.parse('https://webshop-sigma.vercel.app/api/product?slug=&per_page=20&page=&orderby=&order=&pa_color=&range_price=&pa_brand=&pa_discount=&pa_rating=&key=shi'));
 
 // console.log(`  *** editData()`, editData())
 
--- middlewares/errorHandleMdw.js
+++ middlewares/errorHandleMdw.js
@@ -1,7 +1,7 @@
 const errorHandleMdw = (err, req, res, next) => {
    if (err) {
       const{url,body,headers}=req
-      // console.log(`ERROR: `, {url,body,headers,stack:err.stack})
+      console.log(`ERROR: `, {url,body,headers,stack:err.stack})
       res.status(err.status||400).send(err.message);
    } else {
       next();
