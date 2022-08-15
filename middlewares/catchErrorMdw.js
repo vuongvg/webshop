a1aed87839795f173d1cba8c6c670c93d8899a70
vuongvg <vuongcdt1@gmail.com>
2022-08-15 16:04:05 +0700

    fix catchError handleError promise.resolve

--- middlewares/catchErrorMdw.js
+++ middlewares/catchErrorMdw.js
@@ -1,14 +1,14 @@
-// const catchErrorMdw = (status, fn) => {
-//       return async (req, res, next) => {
-//          try {
-//             await fn(req, res, next);
-//          } catch (error) {
-//             next({ message: error.message, stack: error.stack, status });
-//          }
-//       };
+const catchErrorMdw = (status, fn) => {
+      return async (req, res, next) => {
+         try {
+            await fn(req, res, next);
+         } catch (error) {
+            next({ message: error.message, stack: error.stack, status });
+         }
+      };
 
-//     };
-const catchErrorMdw = (status, fn) => (req, res, next) =>
-   Promise.resolve(fn(req, res, next)).catch(next({ message: error.message, stack: error.stack, status }));
+    };
+// const catchErrorMdw = (status, fn) => (req, res, next) =>
+//    Promise.resolve(fn(req, res, next)).catch(next({ message: error.message, stack: error.stack, status }));
 
 module.exports = { catchErrorMdw };
