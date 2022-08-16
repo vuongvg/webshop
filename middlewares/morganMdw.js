9dbfded07b8c7527834f4ba4f4d4ed7b110d9a20
vuongvg <vuongcdt1@gmail.com>
2022-08-16 22:22:56 +0700

    deploy vercel  fix path ../temp/ 6

--- middlewares/morganMdw.js
+++ middlewares/morganMdw.js
@@ -1,37 +1,37 @@
-const fs = require("fs");
-const morgan = require("morgan");
-const path = require("path");
-// path.join(__dirname, "../temp/error.txt")
-const accessLogStream = fs.createWriteStream(path.join(__dirname, "./public/error.txt"), { flags: "a" });
+// const fs = require("fs");
+// const morgan = require("morgan");
+// const path = require("path");
+// // path.join(__dirname, "../temp/error.txt")
+// const accessLogStream = fs.createWriteStream(path.join(__dirname, "./public/error.txt"), { flags: "a" });
 
-const morganMdw = () => {
-   return morgan(
-      (tokens, req, res) => {
-         return [
-            tokens.method(req, res),
-            "-",
-            new Date().toLocaleDateString(),
-            "-",
-            tokens.url(req, res),
-            "-",
-            tokens.status(req, res),
-            "-",
-            tokens.res(req, res, "content-length"),
-            "-",
-            tokens["response-time"](req, res),
-            "ms",
-            req?.error?.stack,
-            '\n'
-         ].join(" ");
-      },
-      {
-         skip: function (req, res) {
-            return res.statusCode < 400;
-         },
-         stream: accessLogStream,
-      }
-   );
-};
-// app.use(morgan("dev",{stream:accessLogStream}));
+// const morganMdw = () => {
+//    return morgan(
+//       (tokens, req, res) => {
+//          return [
+//             tokens.method(req, res),
+//             "-",
+//             new Date().toLocaleDateString(),
+//             "-",
+//             tokens.url(req, res),
+//             "-",
+//             tokens.status(req, res),
+//             "-",
+//             tokens.res(req, res, "content-length"),
+//             "-",
+//             tokens["response-time"](req, res),
+//             "ms",
+//             req?.error?.stack,
+//             '\n'
+//          ].join(" ");
+//       },
+//       {
+//          skip: function (req, res) {
+//             return res.statusCode < 400;
+//          },
+//          stream: accessLogStream,
+//       }
+//    );
+// };
+// // app.use(morgan("dev",{stream:accessLogStream}));
 
-module.exports = { morganMdw };
+// module.exports = { morganMdw };
