3854ab6c491b89ba76a4224c692b36e41cd5ca1a
vuongvg <vuongcdt1@gmail.com>
2022-08-19 00:05:11 +0700

    fix DB..

--- routers/db.js
+++ routers/db.js
@@ -3,10 +3,8 @@ const { connectToDb } = require("../database");
 const { catchErrorMdw } = require("../middlewares/catchErrorMdw");
 const router = express.Router();
 
-router.get(
-   "/",
-   catchErrorMdw(403, async (req, res) => {
-       console.log(`  *** rocess.env.MONGODB_URI`, rocess.env.MONGODB_URI)
+router.get("/",catchErrorMdw(403, async (req, res) => {
+       console.log(`  *** process.env.MONGODB_URI`, process.env.MONGODB_URI)
       try {
          await connectToDb(process.env.MONGODB_URI);
          res.send("DB reconnect");
