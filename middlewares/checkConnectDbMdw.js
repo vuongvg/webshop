83b947576a15c9f16116090fad8d8e546ab9698d
vuongvg <vuongcdt1@gmail.com>
2022-08-19 15:48:53 +0700

    test connect db

--- middlewares/checkConnectDbMdw.js
+++ middlewares/checkConnectDbMdw.js
@@ -1,10 +1,12 @@
 const { db } = require("../database");
 const { customError } = require("../errors/customError");
 const checkConnectDbMdw = async (req, res, next) => {
-
    try {
-      //   db.users || (await connectToDb(process.env.MONGODB_URI));
-      // console.log('DB reconnect');
+      if (!db.users) {
+         console.log("No connect DB");
+         await connectToDb(process.env.MONGODB_URI);
+         console.log("DB reconnect");
+      }
    } catch (error) {
       console.log(error);
       next(customError(503, "DB not reconnect"));
