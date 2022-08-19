a423c76ab5b9bc19bcfbe312a5e43f47db66c9e1
vuongvg <vuongcdt1@gmail.com>
2022-08-19 17:09:28 +0700

    test connect db

--- database/index.js
+++ database/index.js
@@ -4,15 +4,17 @@ const dbName = "webshop";
 const db = {};
 const connectToDb = async (URI) => {
    const mongodbClient = new MongoClient(URI);
-   mongodbClient.on("serverHeartbeatStarted", (event) => {
-      console.log("serverHeartbeatStarted: ", event);
-   });
-   mongodbClient.on("serverHeartbeatSucceeded", (event) => {
-      console.log("serverHeartbeatSucceeded: ", );
-   });
-   mongodbClient.on("serverHeartbeatFailed", (event) => {
-      console.log("serverHeartbeatFailed: ", event);
-   });
+
+   // mongodbClient.on("serverHeartbeatStarted", (event) => {
+   //    console.log("serverHeartbeatStarted: ", event);
+   // });
+   // mongodbClient.on("serverHeartbeatSucceeded", (event) => {
+   //    console.log("serverHeartbeatSucceeded: ", );
+   // });
+   // mongodbClient.on("serverHeartbeatFailed", (event) => {
+   //    console.log("serverHeartbeatFailed: ", event);
+   // });
+   
    await mongodbClient.connect();
    console.log("DB Connected");
    const database = mongodbClient.db(dbName);
