5290851a8cafae3f6627b9bb46b288699e455a16
vuongvg <vuongcdt1@gmail.com>
2022-08-19 13:11:38 +0700

    test connect db

--- middlewares/checkConnectDbMdw.js
+++ middlewares/checkConnectDbMdw.js
@@ -3,7 +3,7 @@ const { customError } = require("../errors/customError");
 let time;
 const checkConnectDbMdw = async (req, res, next) => {
    clearInterval(time);
-   const time = setInterval(() => {
+   time = setInterval(() => {
       console.log("DB: ", db);
    }, 5 * 60 * 1000);
    try {
